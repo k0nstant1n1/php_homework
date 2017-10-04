a14da0b176e0826d54dc0ede152d6b5fa28466a5
konstantin <ziuzinkonstantin@gmail.com>
2017-10-04 23:27:57 +0300

    add update

--- fraction/table.php
+++ fraction/table.php
@@ -10,11 +10,13 @@ $sql = "select fraction1, fraction2, resFraction, operationDate from addFraction
         order by id desc;";
 $res = mysqli_query($conn, $sql);
 echo "<table border='1' width='50%'><tr><th>Дробь1</th><th>Дробь2</th><th>Сумма дробей</th><th>Дата сложения</th></tr>";
-while($row = mysqli_fetch_assoc($res)){
-    echo "<tr><td>{$row['fraction1']}</td>
-              <td>{$row['fraction2']}</td>
-              <td>{$row['resFraction']}</td>
-              <td>{$row['operationDate']}</td></tr>";
+if(gettype($res) != 'boolean'){
+    while($row = mysqli_fetch_assoc($res)){
+        echo "<tr><td>{$row['fraction1']}</td>
+                  <td>{$row['fraction2']}</td>
+                  <td>{$row['resFraction']}</td>
+                  <td>{$row['operationDate']}</td></tr>";
+    }
 }
 echo '</table>';
 mysqli_close($conn);
